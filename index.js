9bf233a6a9f6516606dc09fc82f56869778a2232
Rangel Diaz <60003208+Luis150228@users.noreply.github.com>
2023-06-14 17:29:11 -0600

    Update index.js
    cambios

--- index.js
+++ index.js
@@ -1,98 +1,59 @@
-// Datos de usuario (en una aplicación real, esto debería almacenarse en una base de datos)
-const registrar = document.querySelector("#frm-reg");
-const login = document.querySelector("#frm-log");
+/*
+<div class="mb-3">
+  <label for="formFile" class="form-label">Documentos de ingreso</label>
+  <input required class="form-control" type="file" id="formFileDocs" name="adjunto">
+</div>
+*/
 
-let users = [
-  {
-    username: "lrangel",
-    salt: "2q6591",
-    hashedPassword: "ZmVuaXhmcmVlMnE2NTkx",
-  },
-];
+const inputFile = document.getElementById('formFileDocs');
+const fetchBeta = async (ruta, datos)=>{
+    const data = JSON.stringify(datos);
+    const destino = `${api.url}${ruta}.php`
+    const fetchConfig = {
+        method: "POST",
+        headers: $header,
+        body: data
+    }
 
-// Función para validar el formulario de inicio de sesión
-const validateForm = () => {
-  const usernameInput = document.getElementById("username");
-  const passwordInput = document.getElementById("password");
-  const username = usernameInput.value;
-  const password = passwordInput.value;
+    const peticion = new Request(destino, fetchConfig);
 
-  // Busca el usuario correspondiente al nombre de usuario ingresado
-  const user = users.find((user) => user.username === username);
-  if (!user) {
-    alert("Usuario no encontrado");
-    return false;
-  }
+    if (data instanceof FormData) {
+        const formDataEntries = [...data.entries()];
+        console.log(formDataEntries);
+    }else{
+        console.log(data);
+    }
+}
 
-  // Verifica si la contraseña ingresada es correcta
-  const isValidPassword = verifyPassword(
-    password,
-    user.salt,
-    user.hashedPassword
-  );
-  if (!isValidPassword) {
-    alert("Contraseña incorrecta");
-    return false;
-  }
 
-  // Si el nombre de usuario y la contraseña son válidos, redirige a la página de inicio
-  alert("¡Inicio de sesión exitoso!");
-  return true;
-};
+inputFile.addEventListener('change', async (e) =>{
+        console.log(e);
+        const file = e.target.files[0];
+        const typFile = e.target.files[0].type;
+        const sizFile = ((e.target.files[0].size)/1024)/1024;
+        const refFile = numPedido.value
+        console.log(file);
+        const typeFiles = [
+            "application/pdf",
+            "image/jpeg",
+            "image/png",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        ];
 
-// Función para registrar un nuevo usuario
-const registerUser = () => {
-  const newUsernameInput = document.getElementById("newUsername");
-  const newPasswordInput = document.getElementById("newPassword");
-  const newUsername = newUsernameInput.value;
-  const newPassword = newPasswordInput.value;
+        if (typeFiles.includes(typFile) && sizFile <= 5) {
+            const data = new FormData();
+            data.append('id', refFile);
+            data.append('type', typFile);
+            data.append('archivo', file);
+            console.log([...data]);
+            divLoader()
+            const attachFile = await fetchBeta('adjuntos', data);
+            console.log(attachFile);
+            divLoader()
 
-  // Verifica si el nombre de usuario ya está en uso
-  const userExists = users.some((user) => user.username === newUsername);
-  if (userExists) {
-    alert("El nombre de usuario ya está en uso");
-    return false;
-  }
+        }else{
+            alert('Tipo o tamaño de archivo incorrecto')
+            inputFile.value = ''
+        }
 
-  // Genera una nueva sal y cifra la contraseña con la sal
-  const newSalt = Math.random().toString(36).substring(2, 8);
-  const newHashedPassword = hashPassword(newPassword, newSalt);
-
-  // Agrega el nuevo usuario al arreglo de usuarios
-  users.push({
-    username: newUsername,
-    salt: newSalt,
-    hashedPassword: newHashedPassword,
-  });
-
-  alert("Registro exitoso");
-  return true;
-};
-
-// Función para cifrar la contraseña
-const hashPassword = (password, salt) => {
-  const hash = password + salt;
-  const encrypted = btoa(hash);
-  return encrypted;
-};
-
-// Función para verificar la contraseña
-const verifyPassword = (password, salt, hashedPassword) => {
-  const hash = password + salt;
-  const encrypted = btoa(hash);
-  return encrypted === hashedPassword;
-};
-
-registrar.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  registerUser();
-  console.log(users);
-});
-
-login.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  validateForm();
-  console.log(users);
-});
+    })
