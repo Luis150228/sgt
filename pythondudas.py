49997426d01958dd96314cc3f3b26a590be3354a
Rangel Diaz <60003208+Luis150228@users.noreply.github.com>
2024-07-30 16:15:30 -0600

    sl

--- pythondudas.py
+++ pythondudas.py
@@ -1,6 +1,19 @@
-[Running] python -u "c:\Users\c356882\Desktop\reportsAuto\testSel.py"
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\Temp\chrome-win64')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/')
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
+
+
+[Running] python -u "c:\Users\c356882\Desktop\autoEUT\test001.py"
 Traceback (most recent call last):
-  File "c:\Users\c356882\Desktop\reportsAuto\testSel.py", line 4, in <module>
+  File "c:\Users\c356882\Desktop\autoEUT\test001.py", line 4, in <module>
     driver = webdriver.Chrome('C:\Temp\chrome-win64')  # Optional argument, if not specified will search path.
   File "C:\Users\c356882\AppData\Local\Programs\Python\Python310\lib\site-packages\selenium\webdriver\chrome\webdriver.py", line 45, in __init__
     super().__init__(
