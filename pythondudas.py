9dd11b8aa34f6e848e7ed595a38c4b907d41454a
Rangel Diaz <60003208+Luis150228@users.noreply.github.com>
2024-07-30 15:36:35 -0600

    data

--- pythondudas.py
+++ pythondudas.py
@@ -1,7 +1,13 @@
-[Running] python -u "c:\Users\c356882\Desktop\reportsAuto\selenium.py"
+[Running] python -u "c:\Users\c356882\Desktop\reportsAuto\testSel.py"
 Traceback (most recent call last):
-  File "c:\Users\c356882\Desktop\reportsAuto\selenium.py", line 2, in <module>
-    from selenium import webdriver
-  File "c:\Users\c356882\Desktop\reportsAuto\selenium.py", line 2, in <module>
-    from selenium import webdriver
-ImportError: cannot import name 'webdriver' from partially initialized module 'selenium' (most likely due to a circular import) (c:\Users\c356882\Desktop\reportsAuto\selenium.py)
+  File "c:\Users\c356882\Desktop\reportsAuto\testSel.py", line 4, in <module>
+    driver = webdriver.Chrome('C:\Temp\chrome-win64')  # Optional argument, if not specified will search path.
+  File "C:\Users\c356882\AppData\Local\Programs\Python\Python310\lib\site-packages\selenium\webdriver\chrome\webdriver.py", line 45, in __init__
+    super().__init__(
+  File "C:\Users\c356882\AppData\Local\Programs\Python\Python310\lib\site-packages\selenium\webdriver\chromium\webdriver.py", line 50, in __init__
+    if finder.get_browser_path():
+  File "C:\Users\c356882\AppData\Local\Programs\Python\Python310\lib\site-packages\selenium\webdriver\common\driver_finder.py", line 47, in get_browser_path
+    return self._binary_paths()["browser_path"]
+  File "C:\Users\c356882\AppData\Local\Programs\Python\Python310\lib\site-packages\selenium\webdriver\common\driver_finder.py", line 56, in _binary_paths
+    browser = self._options.capabilities["browserName"]
+AttributeError: 'str' object has no attribute 'capabilities'
